f60ee2541116ecac31e6b648d769148f9ab307fa
iccanobif <iccanobif@fake.mail>
2018-12-26 21:29:49 +0100

    arrow keys in library

--- reactSources/comicLibraryComponent.js
+++ reactSources/comicLibraryComponent.js
@@ -6,11 +6,28 @@ class Filter extends React.Component
     {
         super(props)
         this.handleFilterTextChange = this.handleFilterTextChange.bind(this)
+        this.handleKeyDown = this.handleKeyDown.bind(this)
     }
     handleFilterTextChange(e)
     {
         this.props.onFilterTextChange(e.target.value)
     }
+    handleKeyDown(e)
+    {
+        switch (e.key)
+        {
+            case "ArrowDown":
+                this.props.onMoveDown()
+                break;
+            case "ArrowUp":
+                this.props.onMoveUp()
+                break;
+            case "Enter":
+                this.props.onSelectionConfirmed()
+                break;
+        }
+    }
+
     render() 
     {
         return (
@@ -20,6 +37,7 @@ class Filter extends React.Component
                     placeholder="Search..."
                     value={this.props.filterText}
                     onChange={this.handleFilterTextChange}
+                    onKeyDown={this.handleKeyDown}
                 />
             </form>
         )
@@ -42,7 +60,7 @@ class ComicList extends React.Component
                         return <a
                             key={comic.title}
                             href="#"
-                            className={i == this.props.selectedIndex ? "comicLink" : "comicLink"}
+                            className={i == this.props.selectedIndex ? "selectedComicLink" : "comicLink"}
                             onClick={(e) =>
                             {
                                 e.preventDefault()
@@ -67,12 +85,40 @@ class FilterableComicList extends React.Component
             selectedIndex: 0
         }
         this.handleFilterTextChange = this.handleFilterTextChange.bind(this)
+        this.handleMoveUp = this.handleMoveUp.bind(this)
+        this.handleMoveDown = this.handleMoveDown.bind(this)
+        this.handleSelectionConfirmed = this.handleSelectionConfirmed.bind(this)
     }
     handleFilterTextChange(filterText)
     {
         this.setState({
             filterText: filterText
         })
+    }
+    handleMoveUp()
+    {
+        this.setState({
+            selectedIndex: Math.max(0, this.state.selectedIndex - 1)
+        })
+    }
+    handleMoveDown()
+    {
+        this.setState({
+            selectedIndex: this.state.selectedIndex + 1 // TODO prevent going out of bounds
+        })
+    }
+    handleSelectionConfirmed()
+    {
+        const selectedComic = this.getFilteredComicList()[this.state.selectedIndex]
+        this.props.onComicSelected(selectedComic)
+    }
+    getFilteredComicList()
+    {
+        return this.props.comicList
+            .filter(comic =>
+            {
+                return comic.title.toUpperCase().indexOf(this.state.filterText.toUpperCase()) >= 0
+            })
     }
     render()
     {
@@ -81,13 +127,12 @@ class FilterableComicList extends React.Component
                 <Filter
                     filterText={this.state.filterText}
                     onFilterTextChange={this.handleFilterTextChange}
+                    onMoveDown={this.handleMoveDown}
+                    onMoveUp={this.handleMoveUp}
+                    onSelectionConfirmed={this.handleSelectionConfirmed}
                 />
                 <ComicList
-                    comicList={this.props.comicList
-                        .filter(comic =>
-                        {
-                            return comic.title.toUpperCase().indexOf(this.state.filterText.toUpperCase()) >= 0
-                        })}
+                    comicList={this.getFilteredComicList()}
                     selectedIndex={this.state.selectedIndex}
                     onComicSelected={this.props.onComicSelected}
                 />
